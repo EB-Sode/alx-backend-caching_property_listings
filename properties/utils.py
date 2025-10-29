c8540c4b9f4ee4fdcfcfcf67cf7d4a288871d4d1
Blessed Sode <emi.square23@gmail.com>
2025-10-29 14:15:55 +0100

    when

--- properties/utils.py
+++ properties/utils.py
@@ -6,11 +6,10 @@ def get_all_properties():
     properties = cache.get('all_properties')
 
     if properties is None:
-        print("Fetching from database...")  # for debugging
-        properties = list(Property.objects.values())
-        # Cache the queryset for 1 hour (3600 seconds)
+        print("Fetching from database...")
+        properties = Property.objects.all().values()
         cache.set('all_properties', properties, 3600)
     else:
-        print("Fetching from cache...")  # for debugging
+        print("Fetching from cache...")
 
     return properties
