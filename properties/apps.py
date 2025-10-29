5121fc32e3968436408dea49319802c1dad23a8b
Blessed Sode <emi.square23@gmail.com>
2025-10-29 14:24:17 +0100

    when

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,10 @@
-from django.apps import AppConfig
 
+from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        # Import signal handlers
+        import properties.signals
